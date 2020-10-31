8878110a3fae16853d5da8b7513cae22571aec46
Xavier0301 <xavier.servot0@gmail.com>
2020-10-31 17:37:47 +0100

    Adds panel data loader

--- openpifpaf/functional.c
+++ openpifpaf/functional.c
@@ -5,7 +5,7 @@
     "distutils": {
         "depends": [],
         "include_dirs": [
-            "/Users/svenkreiss/tech/openpifpaf/venv3/lib/python3.8/site-packages/numpy/core/include"
+            "/Users/xavier/miniconda3/lib/python3.7/site-packages/numpy/core/include"
         ],
         "name": "openpifpaf.functional",
         "sources": [
